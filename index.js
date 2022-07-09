d9062a0958fe6dcd399e4d6cd3788bb81d5d5f1e
Lafon Christophe <lfnchrstph@MacBook-Pro-de-Lafon.local>
2022-07-09 20:12:22 +0200

    fef

--- index.js
+++ index.js
@@ -202,11 +202,6 @@ app.post("/offer/publish", isAuthenticated, async (req, res) => {
         picturesToUpload.push(req.files.picture[i]);
       }
     }
-    return res.json({
-      message0: req.files.picture,
-      message1: picturesToUpload,
-      message2: req.files.picture.length,
-    });
     let buffersToUpload = [];
     for (i = 0; i < picturesToUpload.length; i++) {
       let picToUl = picturesToUpload[i];
